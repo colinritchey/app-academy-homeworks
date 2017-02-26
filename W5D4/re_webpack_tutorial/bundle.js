6f3bc4388b5072f1fb5200deeb51ddd2a9098296
Colin Ritchey <colinritchey256@gmail.com>
2017-02-25 19:50:59 -0800

    All exercises completed

--- W5D4/re_webpack_tutorial/bundle.js
+++ W5D4/re_webpack_tutorial/bundle.js
@@ -77,7 +77,7 @@ module.exports = "It works from content.js.";
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-// require("!style!css!./style.css");
+__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"!style!css!./style.css\""); e.code = 'MODULE_NOT_FOUND';; throw e; }()));
 document.write(__webpack_require__(0));
 
 
